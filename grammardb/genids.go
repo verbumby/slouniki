2378c81b736c0ec336e8bd69b3a57740e36fc02f
Vadzim Ramanenka <vramanenka@gmail.com>
2025-11-11 17:56:46 +0100

    Абнавіў ГрамБазу да 2025 версіі

--- grammardb/genids.go
+++ grammardb/genids.go
@@ -14,12 +14,31 @@ type rowT struct {
 	FK     string
 	Tag    string
 	Lemma  string
-	Suffix string
+	Suffix *string
 }
 
 const DirName = "."
 
+func load23ids() map[string]rowT {
+	ids23f, err := os.Open("./ids-2023.txt")
+	if err != nil {
+		panic(err)
+	}
+	s := bufio.NewScanner(ids23f)
+
+	ans := map[string]rowT{}
+	for s.Scan() {
+		ps := strings.Split(s.Text(), "\t")
+		id, tag, lemma, suffix := ps[0], ps[1], ps[2], ps[3]
+		ans[id] = rowT{FK: id, Tag: tag, Lemma: lemma, Suffix: &suffix}
+	}
+
+	return ans
+}
+
 func main() {
+	ids23 := load23ids()
+
 	xmls, err := os.ReadDir(DirName)
 	if err != nil {
 		panic(err)
@@ -64,27 +83,54 @@ func main() {
 				fk := paradigmXML.Attrs["pdgId"] + "-" + variantXML.Attrs["id"]
 
 				totals[lemma]++
-				rows = append(rows, rowT{
+				row := rowT{
 					FK:    fk,
 					Tag:   tag,
 					Lemma: lemma,
-				})
+				}
+
+				if row23, ok := ids23[row.FK]; ok {
+					if row.Tag != row23.Tag || row.Lemma != row23.Lemma {
+						// fmt.Printf("tag or lemma do not match: %v vs %v\n", row, row23)
+					} else {
+						row.Suffix = row23.Suffix
+					}
+				}
+				rows = append(rows, row)
 			}
 		}
 	}
 
-	counts := map[string]int{}
+	suffixes := map[string]bool{}
 	for i := range rows {
-		lemma := rows[i].Lemma
-		if totals[lemma] == 1 {
+		if rows[i].Suffix == nil {
 			continue
 		}
-		counts[lemma]++
-		rows[i].Suffix = strconv.FormatInt(int64(counts[lemma]), 10)
+		suff := *(rows[i].Suffix)
+		if suff == "" {
+			suff = "1"
+		}
+		suffixes[rows[i].Lemma+"-"+suff] = true
+	}
+
+	for i := range rows {
+		n := 1
+		for rows[i].Suffix == nil {
+			ns := strconv.FormatInt(int64(n), 10)
+			id := rows[i].Lemma + "-" + ns
+			if !suffixes[id] {
+				suffixes[id] = true
+				if ns == "1" {
+					ns = ""
+				}
+				rows[i].Suffix = &ns
+			}
+			n++
+		}
 	}
 
 	for _, row := range rows {
-		fmt.Printf("%s\t%s\t%s\t%s\n", row.FK, row.Tag, row.Lemma, row.Suffix)
-		fmt.Fprintf(out, "%s\t%s\t%s\t%s\n", row.FK, row.Tag, row.Lemma, row.Suffix)
+		fmt.Printf("%s\t%s\t%s\t%s\n", row.FK, row.Tag, row.Lemma, *row.Suffix)
+		fmt.Fprintf(out, "%s\t%s\t%s\t%s\n", row.FK, row.Tag, row.Lemma, *row.Suffix)
 	}
 }
